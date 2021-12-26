828b65232dc99e67b1401ff5b1d952f2a598098e
islamsamy214 <islamsamy214@gmail.com>
2021-12-26 12:18:08 +0200

    update commit

--- breadth-first-search.php
+++ breadth-first-search.php
@@ -1,57 +1,76 @@
-<?php 
+<?php
 
-class BFS{
+class BFS
+{
     private $graph = [];
     private $search_queue = [];
     private $searched = [];
     private $family = [];
 
-    public function __construct(){
-        $this->graph['me'] = ['jerry','spike','tom'];
-        $this->graph['jerry'] = ['snoop dog', 'g-ezi','tom'];
-        $this->graph['tom'] = ['leonardo decaprio', 'alpacino','spike'];
+    public function __construct()
+    {
+        $this->graph['me'] = ['spike', 'tom', 'jerry'];
+        $this->graph['jerry'] = ['snoop dog', 'g-ezi', 'tom'];
+        $this->graph['tom'] = ['leonardo decaprio', 'alpacino', 'spike'];
         $this->graph['spike'] = ['his little boy'];
-    }
+    }//end of construct
 
-    public function search($whoseSearching){
+    public function search($whoseSearching)
+    {
         $this->search_queue = $this->graph[$whoseSearching];
+
         while ($this->search_queue) {
+
             $person = array_shift($this->search_queue);
-            if(!array_search($person,$this->searched)){
-                
-                if ($this->is_drag_deller($person)){
-                    echo $person . ' is a drug deller';
-                    print_r($this->parents($person));
+
+            if (!array_search($person, $this->searched)) {
+
+                if ($this->isDrugDealer($person)) {
+
+                    echo $person . ' is a drug dealer and i reahced to him ';
+                    //to detect the path
+                    foreach ($this->parents($person) as $parent) {
+                        echo ' throw ' . $parent;
+                    }
                     return true;
                 }
-                
-                $this->search_queue = array_merge($this->search_queue,$this->graph[$person]);
-                array_unshift($this->searched,$person);
+                if (isset($this->graph[$person])) {
+                    $this->search_queue = array_merge($this->search_queue, $this->graph[$person]);
+                }
+
+                array_unshift($this->searched, $person);
             }
         }
 
         echo 'it seems like i found no one';
         return false;
-    }
+    }//end of search
 
-    public function is_drag_deller($person){
+    public function isDrugDealer($person)
+    {
         return $person == 'snoop dog' ? true : false;
-    }
+    } //end of is_drag_deller
 
-    public function parents($person){
-        foreach ($this->searched as $parent){
-            
-            if(array_search($person,$this->graph[$parent])){
-                array_unshift($this->family,$parent);
+    //to detect the, how i reached to this element
+    public function parents($person)
+    {
+        foreach ($this->searched as $parent) {
+            if ($this->hasParent($parent, $person)) {
+
+                array_unshift($this->family, $parent);
+
                 $this->parents($parent);
             }
         }
+
         return $this->family;
-        
-    }
+    } //end of parents
+
+    public function hasParent($parent, $person)
+    {
+        return isset($this->graph[$parent]) && is_int(array_search($person, $this->graph[$parent]));
+    }//end of hasParent
 }
 
 $testingBoy = new BFS();
 $testingBoy->search('me');
-
-?>
